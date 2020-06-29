209dc556155d59be2ca6a4e997d47f956838b874
Nathan <nathanmcneil@pm.me>
2020-06-28 23:02:39 -0700

    layed out nodeJS files

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    search: '',
+    
+  }
+  render () {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default App
